7ac7e0acb90f42404df03423caf46725fa23d477
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-05-01 01:54:33 +0200

    Defined userActions

---
+++ src/simple-redux/userActions.ts
@@ -0,0 +1,31 @@
+import * as actionTypes from "./actionTypes";
+import { getUsers } from "../api/user";
+
+export const loadingUsers = () => {
+  return { type: actionTypes.LOADING_USERS };
+};
+
+export const loadingUsersSucces = (users: any) => {
+  return {
+    type: actionTypes.LOADING_USERS_SUCCESS,
+    users,
+  };
+};
+
+export const loadingUsersError = (error: string) => {
+  return {
+    type: actionTypes.LOADING_USERS_ERROR,
+    error,
+  };
+};
+
+export const loadUsers = async (dispatch: any) => {
+  dispatch(loadingUsers());
+  try {
+    const [error, users] = await getUsers();
+    if (error) return dispatch(loadingUsersError(error));
+    dispatch(loadingUsersSucces(users));
+  } catch (err) {
+    return dispatch(loadingUsersError("Unexpected error"));
+  }
+};
