8fa486a6fe78ccf04b11cb38ba492bc4746e0406
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-04-30 23:12:09 +0200

    Added the api folder with user endpoint

---
+++ src/api/user.test.ts
@@ -0,0 +1,7 @@
+import { getUsers } from "./user";
+
+describe("getUsers", () => {
+  it("is defined", () => {
+    expect(getUsers).toBeDefined();
+  });
+});
---
+++ src/api/user.ts
@@ -0,0 +1,31 @@
+import { fetch } from "../utils";
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+const resource = "users";
+
+export const getUsers = fetch(
+  `${process.env.REACT_APP_JSON_PLACEHOLDER_API_URL}${resource}`
+);
