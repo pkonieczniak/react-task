62c20148b45c86974eea5f154095fed89d65d337
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-04-30 22:42:43 +0200

    Added tests when the request succeed

--- src/utils/fetch.test.ts
+++ src/utils/fetch.test.ts
@@ -4,11 +4,31 @@ describe("fetch", () => {
   describe("when the error occured", () => {
     it("contains error message", async () => {
       const fakeError = Promise.reject(new Error("Internal Server error"));
-      window.fetch = jest.fn(() => fakeError);
+
+      window.fetch = jest.fn().mockImplementation(() => fakeError);
 
       const [error, result] = await fetch("/foo/bar");
       expect(error).toEqual("Internal Server error");
       expect(result).toBeUndefined();
+
+      jest.clearAllMocks();
+    });
+  });
+
+  describe("when the request succeed", () => {
+    it("contains response data", async () => {
+      const fakeResponse = Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ foo: "bar" }),
+      });
+
+      window.fetch = jest.fn().mockImplementation(() => fakeResponse);
+
+      const [error, result] = await fetch("/foo/bar");
+      expect(error).toBeUndefined();
+      expect(result).toStrictEqual({ foo: "bar" });
+
+      jest.clearAllMocks();
     });
   });
 });
