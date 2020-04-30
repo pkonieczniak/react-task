09d95849a8f7334730f88fbd36e5fbcdb98ae9f4
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-04-30 22:10:45 +0200

    Added fetch util

---
+++ src/utils/fetch.test.ts
@@ -0,0 +1,5 @@
+import { fetch } from "./fetch";
+
+it("is defined", () => {
+  expect(fetch).toBeDefined();
+});
---
+++ src/utils/fetch.ts
@@ -0,0 +1,28 @@
+interface Fetch {
+  (
+    url: string,
+    options: {
+      method: string;
+    }
+  ): Promise<[string?, any?]>;
+}
+
+export const fetch: Fetch = async (url, options) => {
+  const { method = "GET" } = options;
+
+  let result, error;
+
+  try {
+    const response = await window.fetch(url, {
+      method,
+      headers: { "Content-Type": "application-json" },
+    });
+
+    if (!response.ok) error = await response.text();
+    result = await response.json();
+  } catch (err) {
+    error = err.message;
+  }
+
+  return [error, result];
+};
