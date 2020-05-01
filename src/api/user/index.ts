3b6853ea776ce85f0e3b7134336af4f9244fbcc7
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-05-01 07:57:51 +0200

    Import User model to the user api

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,28 +1,5 @@
 import { fetch } from "../../utils";
-
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-}
+import { User } from "../../models/user";
 
 const resource = "users";
 
