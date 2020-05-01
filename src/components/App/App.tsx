052cda3d679b2a1a885034133c62a8e8d0893f8b
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-05-01 03:12:56 +0200

    Added list filtering

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useReducer, useEffect } from "react";
+import React, { FC, useReducer, useEffect, ChangeEvent, useState } from "react";
 
 import { userReducer } from "../../simple-redux/userReducer";
 import { initialState } from "../../simple-redux/initialState";
@@ -7,19 +7,27 @@ import { loadUsers } from "../../simple-redux/userActions";
 import "./App.css";
 
 import { UsersList } from "../users";
+import { Input } from "../common/input";
 
 export const App: FC<any> = () => {
   const [state, dispatch] = useReducer(userReducer, initialState);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     loadUsers(dispatch);
   }, []);
 
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div>
       <h1>Users</h1>
+      <Input name="users" type="text" onChange={onChange} />
       <UsersList
         users={state.users}
+        inputValue={inputValue}
         error={state.error}
         isLoading={state.isLoading}
       />
--- src/components/users/UsersList.tsx
+++ src/components/users/UsersList.tsx
@@ -7,9 +7,16 @@ export const UsersList: FC<any> = (props) => {
   if (!props.users || !props.users.length)
     return <p>No users found based on the search criteria. Try again</p>;
 
+  let filteredList = [...props.users];
+  if (props.inputValue) {
+    filteredList = props.users.filter((user: any) =>
+      user.name.toLowerCase().includes(props.inputValue.toLowerCase())
+    );
+  }
+
   return (
     <ol>
-      {props.users.map((user: any) => {
+      {filteredList.map((user: any) => {
         return <li key={user.id}>{user.name}</li>;
       })}
     </ol>
